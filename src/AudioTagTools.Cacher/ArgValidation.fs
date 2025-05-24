ccebc1d4114e4d9f558d3555791394039748ba0f
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-05-24 23:13:36 +0900

    Refactor arg validation function

--- src/AudioTagTools.Cacher/ArgValidation.fs
+++ src/AudioTagTools.Cacher/ArgValidation.fs
@@ -4,6 +4,6 @@ open Errors
 open System.IO
 
 let validate (args: string array) : Result<DirectoryInfo * FileInfo, Error> =
-    if args.Length = 2
-    then Ok (DirectoryInfo args[0], FileInfo args[1])
-    else Error InvalidArgCount
+    match args with
+    | [| a; b |] -> Ok (DirectoryInfo a, FileInfo b)
+    | _ -> Error InvalidArgCount
