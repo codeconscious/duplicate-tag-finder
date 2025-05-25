a191ef10434899cf1e4ad2dcd9be0b6598075ffe
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-05-25 17:51:42 +0900

    Refactor main pattern matching

--- src/AudioTagTools.Main/Program.fs
+++ src/AudioTagTools.Main/Program.fs
@@ -13,26 +13,24 @@ let main args =
 
     match args with
     | [| |] ->
-        let commands = commandMap |> Map.keys |> (fun x -> String.Join("\" or \"", x))
+        let commands = commandMap |> Map.keys |> String.concat "\" or \""
         printfn $"You must pass in a supported command: \"{commands}\"."
         1
     | _ ->
         let command = args[0]
         let flags = args[1..]
 
-        commandMap
-        |> Map.tryFind command
-        |> function
-            | Some fn ->
-                match fn flags with
-                | Ok msg ->
-                    printfn $"{msg}"
-                    printfn $"Done in {watch.ElapsedFriendly}."
-                    0
-                | Error msg ->
-                    printfn $"{msg}"
-                    printfn $"Failed after {watch.ElapsedFriendly}."
-                    1
-            | None ->
-                printfn $"Invalid command \"{command}\". You must pass in a supported command."
+        match Map.tryFind command commandMap with
+        | Some requestedOperation ->
+            match requestedOperation flags with
+            | Ok msg ->
+                printfn $"{msg}"
+                printfn $"Done in {watch.ElapsedFriendly}."
+                0
+            | Error msg ->
+                printfn $"{msg}"
+                printfn $"Failed after {watch.ElapsedFriendly}."
                 1
+        | None ->
+            printfn $"Invalid command \"{command}\". You must pass in a supported command."
+            1
