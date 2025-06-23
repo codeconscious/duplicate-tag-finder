fb983d4cac61c06e9b2471110071af4d8a2d7aeb
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-06-23 18:23:01 +0900

    Merge conflict

--- src/AudioTagTools.DuplicateFinder/IO.fs
+++ src/AudioTagTools.DuplicateFinder/IO.fs
@@ -1,8 +1,12 @@
 module IO
 
+open System
+open System.Text
 open Errors
+open Settings
 open System.IO
 open AudioTagTools.Shared.IO
+open FsToolkit.ErrorHandling
 
 let readFile (fileInfo: FileInfo) : Result<string, Error> =
     readFile fileInfo
@@ -46,43 +50,3 @@ let savePlaylist
     writeTextToFile fullPath (contents.ToString())
     |> Result.map (fun _ -> fullPath)
     |> Result.mapError WriteFileError
-
-let savePlaylist
-    (settings: SettingsRoot)
-    (tags: Map<string, TagLibrary.FilteredTagCollection>)
-    : Result<string, Error>
-    =
-    let mutable contents = StringBuilder("#EXTM3U\n")
-    let now = DateTime.Now.ToString("yyyyMMdd_HHmmss")
-    let filename = $"Duplicates by AudioTagTools - {now}.m3u"
-    let fullPath = Path.Combine(settings.Playlist.SaveDirectory, filename)
-
-    let combine (x: string array) (y: string array) =
-        let xy = Array.append x y
-        String.Join("; ", xy)
-
-    let searchFor, replaceWith = settings.Playlist.SearchPath, settings.Playlist.ReplacePath
-
-    let update (m: TagLibrary.FileTags) : unit =
-        let seconds = m.Duration.TotalSeconds
-        let artist = combine m.AlbumArtists m.Artists
-        let artistTitle = $"{artist} - {m.Title}"
-        let extInf = $"#EXTINF:{seconds},{artistTitle}"
-        contents.AppendLine extInf |> ignore
-
-        let updatedPath =
-            match searchFor, replaceWith with
-            | s, _ when String.IsNullOrEmpty s -> m.FileNameOnly
-            | s, r -> m.FileNameOnly.Replace(s, r)
-
-        contents.AppendLine updatedPath |> ignore
-
-    tags.Values
-    |> Seq.collect id
-    |> Seq.iter update
-
-    writeTextToFile fullPath (contents.ToString())
-    |> Result.map (fun _ -> fullPath)
-    |> Result.mapError WriteFileError
-
-
