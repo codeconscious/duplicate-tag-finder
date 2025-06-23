fcb26c744d4222177c44ec69e7df2c36571e5c0b
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-06-23 17:34:16 +0900

    Initial version

--- src/AudioTagTools.DuplicateFinder/Errors.fs
+++ src/AudioTagTools.DuplicateFinder/Errors.fs
@@ -3,11 +3,13 @@ module Errors
 type Error =
     | InvalidArgCount
     | ReadFileError of string
+    | WriteFileError of string
     | SettingsParseError of string
     | TagParseError of string
 
 let message = function
     | InvalidArgCount -> "Invalid arguments. Pass in two JSON file paths: (1) your settings file and (2) your tag library."
     | ReadFileError msg -> $"Read failure: {msg}"
+    | WriteFileError msg -> $"Write failure: {msg}"
     | SettingsParseError msg -> $"Unable to parse the settings file: {msg}"
     | TagParseError msg -> $"Unable to parse the tag library file: {msg}"
