d2a9ea7a8405246555b9cbd10e0545b76decb95b
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-06-27 18:37:13 +0900

    Refactor savePlaylist function

--- src/AudioTagTools.DuplicateFinder/IO.fs
+++ src/AudioTagTools.DuplicateFinder/IO.fs
@@ -10,25 +10,21 @@ open System.IO
 open FsToolkit.ErrorHandling
 
 let readFile (fileInfo: FileInfo) : Result<string, Error> =
-    readFile fileInfo
+    fileInfo
+    |> readFile
     |> Result.mapError ReadFileError
 
 let savePlaylist (settings: SettingsRoot) (tags: FileTags array array) : Result<unit, Error> =
-    let mutable contents = StringBuilder("#EXTM3U\n")
     let now = DateTime.Now.ToString("yyyyMMdd_HHmmss")
     let filename = $"Duplicates by AudioTagTools - {now}.m3u"
     let fullPath = Path.Combine(settings.Playlist.SaveDirectory, filename)
 
-    let combine (x: string array) (y: string array) =
-        let xy = Array.append x y
-        String.Join("; ", xy)
-
-    let update (m: FileTags) : unit =
+    let appendFileEntry (builder: StringBuilder) (m: FileTags) : StringBuilder =
         let seconds = m.Duration.TotalSeconds
-        let artist = combine m.AlbumArtists m.Artists
-        let artistTitle = $"{artist} - {m.Title}"
-        let extInf = $"#EXTINF:{seconds},{artistTitle}"
-        contents.AppendLine extInf |> ignore
+        let artist = Array.append m.AlbumArtists m.Artists |> String.concat "; "
+        let artistWithTitle = $"{artist} - {m.Title}"
+        let extInf = $"#EXTINF:{seconds},{artistWithTitle}"
+        builder.AppendLine extInf |> ignore
 
         let fullPath = Path.Combine(m.DirectoryName, m.FileNameOnly)
 
@@ -37,12 +33,13 @@ let savePlaylist (settings: SettingsRoot) (tags: FileTags array array) : Result<
             | s, _ when String.IsNullOrEmpty s -> fullPath
             | s, r -> fullPath.Replace(s, r)
 
-        contents.AppendLine updatedPath |> ignore
+        builder.AppendLine updatedPath |> ignore
+        builder
 
     tags
     |> Seq.collect id
-    |> Seq.iter update
-
-    writeTextToFile fullPath (contents.ToString())
+    |> Seq.fold appendFileEntry (StringBuilder("#EXTM3U\n"))
+    |> _.ToString()
+    |> writeTextToFile fullPath
     |> Result.tee (fun _ -> printfn $"Created playlist file \"{fullPath}\".")
     |> Result.mapError WriteFileError
